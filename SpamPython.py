32780b3de2e7c845eba1e63b04b7e6d0789cd3c6
Carlos Daniel de Oliveira Vaz <101210018+cdovaz@users.noreply.github.com>
2023-05-10 18:02:14 -0400

    Add files via upload

---
+++ SpamPython.py
@@ -0,0 +1,8 @@
+
+import pyautogui
+import time
+time.sleep(1.5)
+for i in range(200):
+    pyautogui.typewrite("@Marcus Felipe I1a")
+    #pyautogui.hotkey('ctrl', 'v') 
+    pyautogui.press("enter")
